f60022c44f4fbb06f3c836bca14c1aefd58b117d
emil <emilbh@gmail.com>
2018-08-24 23:01:20 +0200

    Made it better! Now displaying what ever you input!

--- main.py
+++ main.py
@@ -2,8 +2,37 @@ import tkinter as tk
 
 def main():
 
+	
 	win = tk.Tk()
+	win.title("Welcome")
+	win.geometry("500x300")
+
+	# Functions
+	def disp_name():
+		name = str(ent1.get())
+		txt = tk.Text(master=win, height=20,width=25)
+		txt.grid(column=0,row=1, columnspan=2)
+		txt.insert(tk.END, name)
+
+	# Labels
+	lb1 = tk.Label(text="Give me a name")
+	lb1.grid(column=0,row=0)
+
+	# Entry
+	ent1 = tk.Entry()
+	ent1.grid(column=1,row=0)
+
+	# Button
+	btn = tk.Button(text="Push Me",command=disp_name)
+	btn.grid(column=2,row=0)
+
+
+
+
+
 	win.mainloop()
+
+
 
 if __name__ == "__main__":
 	main()
