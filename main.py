c725f94f9a1eb2688c6d13831fba4495aff9fa6c
emiludenz <emilbh@gmail.com>
2018-08-24 21:24:58 +0200

    Major Change, now with label!

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import tkinter as tk
 def main():
 
 	win = tk.Tk()
+
+	# Major Change
+	lableName = tk.Label(text="Heello")
+	lableName.grid(row=0,column=0)
+
 	win.mainloop()
 
 if __name__ == "__main__":
